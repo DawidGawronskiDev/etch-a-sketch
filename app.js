75be1e7b1bf6901206663518fc1bef5bdab5e40b
Dawid Gawronski <dawidgawronskicontact@gmail.com>
2022-08-20 21:38:26 +0100

    Add change color function

--- app.js
+++ app.js
@@ -4,22 +4,30 @@ let containerSize = 256;
 //number of elements in a row
 let elementSize = 8;
 
+//default color
+let colorPick = `hsl(120,100%,50%)`;
+
 let container = document.querySelector("#container");
 container.style.height = 256 + "px";
 container.style.width = 256 + "px";
 
+colorPick = "purple";
 function changeColor(e) {
-  e.target.style.background = "blue";
+  e.target.style.background = colorPick;
+}
+function changeColorSample() {
+  let colorInput = document.querySelector("#colorInput");
+  colorPick = `hsl(` + colorInput.value + `, 100%, 50%)`;
+  console.log(colorInput.value);
 }
 
+//create board
 for (let i = 0; i < elementSize * elementSize; i++) {
   let content = document.createElement("div");
   content.style.width = Math.floor(containerSize / elementSize) + "px";
   content.style.height = Math.floor(containerSize / elementSize) + "px";
-  content.style.background = "red";
-  content.style.outline = "1px solid rgb(0% 0% 0% / .1)";
-
-  content.addEventListener("mousedown", changeColor);
-
+  content.style.background = "rgba(0%, 0%, 0%, .1)";
   container.appendChild(content);
+
+  content.addEventListener("mouseenter", changeColor);
 }
